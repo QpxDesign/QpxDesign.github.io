837556565e59437d1f9c90427fc9b6a29fdd3868
Qpx Design <71609644+QpxDesign@users.noreply.github.com>
2021-12-26 15:29:58 -0500

    Delete _site directory

--- _site/finalgradecalculator/finalgradecalculator.js
+++
@@ -1,7 +0,0 @@
-function neededGradeCalculator() {
-    var currentGrade = Number(document.getElementById("CurrentGrade").value)
-    var finalWorth = Number(document.getElementById("finalWorth").value)
-    var WantedGrade = Number(document.getElementById("WantedGrade").value)
-    var result = WantedGrade - currentGrade * 100-finalWorth/finalWorth
-    alert(result)
-}
--- _site/probabilitycalculator/probabilitycalculator.js
+++
@@ -1,6 +0,0 @@
-function xoutOfChance() {
-    var a  = Number(document.getElementById("chanceA").value);
-    var b  = Number(document.getElementById("chanceAoutOf").value);
-    var result = a/b * 100+ "%";
-    document.getElementById("xoutOfChanceDISPLAY").innerHTML = result;
-}
